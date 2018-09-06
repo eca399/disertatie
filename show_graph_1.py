ae6a87593a7c0a7890ed806ae34e0b16506597a6
ecaterina <ecaterina.parlog@esolutions.ro>
2018-09-06 23:49:28 +0300

    plot intersections

--- show_graph_1.py
+++ show_graph_1.py
@@ -19,20 +19,65 @@ df_gps = pd.read_csv('user_location_with_zones.csv')
 df_clustered = pd.read_csv('user-location-clustered.csv')  
 
 kstartPoint = 0
-kendPoint = 1
-kindex = 0
+kendPoint = 3
+kindex = 1
 # import pdb; pdb.set_trace()
-oxList = [elem['lat'] for elem in transition_mat[kstartPoint][kendPoint]['routes'][kindex]['points']]
-print(oxList)
-oYList = [elem['lon'] for elem in transition_mat[kstartPoint][kendPoint]['routes'][kindex]['points']]
-print(oYList)
-# oxList
 
-fig, ax = plt.subplots(figsize=[11, 8])
-# rs_scatter = ax.scatter(oxList, oYList, c='m', edgecolor='None', alpha=0.3, s=120)
-df_scatter = ax.scatter(oxList, oYList, c='k', alpha=0.5, s=3)
-ax.set_title('Full data set vs DBSCAN reduced set')
-ax.set_xlabel('Longitude')
-ax.set_ylabel('Latitude')
-ax.legend([df_scatter], ['GPS Points'], loc='upper left')
+# with open('Intersections.txt', 'a') as f:
+#     for i in range(df_clustered.shape[0]):
+#         for j in range(df_clustered.shape[0]):
+#             for index in range(len(transition_mat[i][j]['routes'])):
+#                 no_inters = transition_mat[i][j]['routes'][index]['intersections']
+#                 f.write("elem[{}][{}] route[{}] has intersections: {}\n".format(i, j, index, no_inters))
+
+x = [elem['lon'] for elem in transition_mat[kstartPoint][kendPoint]['routes'][kindex]['points']]
+print(x)
+y = [elem['lat'] for elem in transition_mat[kstartPoint][kendPoint]['routes'][kindex]['points']]
+print(y)
+# oxList
+names = [str(elem['index']) for elem in transition_mat[kstartPoint][kendPoint]['routes'][kindex]['points']]
+
+norm = plt.Normalize(1,4)
+cmap = plt.cm.RdYlGn
+
+fig,ax = plt.subplots(figsize=[11, 8])
+sc = plt.scatter(x,y,c='b', s=5, cmap=cmap, norm=norm)
+
+annot = ax.annotate("", xy=(0,0), xytext=(20,20),textcoords="offset points",
+                    bbox=dict(boxstyle="round", fc="w"),
+                    arrowprops=dict(arrowstyle="->"))
+annot.set_visible(False)
+
+def update_annot(ind):
+
+    pos = sc.get_offsets()[ind["ind"][0]]
+    annot.xy = pos
+    text = "{}".format(" ".join([names[n] for n in ind["ind"]]))
+    annot.set_text(text)
+    annot.get_bbox_patch().set_facecolor('b')
+    annot.get_bbox_patch().set_alpha(0.4)
+
+
+def hover(event):
+    vis = annot.get_visible()
+    if event.inaxes == ax:
+        cont, ind = sc.contains(event)
+        if cont:
+            update_annot(ind)
+            annot.set_visible(True)
+            fig.canvas.draw_idle()
+        else:
+            if vis:
+                annot.set_visible(False)
+                fig.canvas.draw_idle()
+
+fig.canvas.mpl_connect("motion_notify_event", hover)
+
+#plt.show()
+x1, y1 = [44.419223, 44.419514], [26.046114, 26.048614]
+# x2, y2 = [1, 10], [3, 2]
+no_inters = transition_mat[kstartPoint][kendPoint]['routes'][kindex]['intersections']
+ratio = transition_mat[kstartPoint][kendPoint]['routes'][kindex]['ratio'] 
+ax.set_title(' of intersections: {} and ratio: {}'.format(no_inters,ratio))
+plt.plot(x,y, marker = 'o')
 plt.show()
--- test.py
+++ test.py
@@ -1,45 +1,48 @@
-import pandas as pd, numpy as np, matplotlib.pyplot as plt, time
-from geopy.distance import great_circle
-# from shapely.geometry import MultiPoint
-from datetime import datetime as dt
-from math import radians, cos, sin, asin, sqrt, atan2
+import matplotlib.pyplot as plt
+import numpy as np; np.random.seed(1)
 
-# define the number of kilometers in one radian
-kms_per_radian = 6371.0088
+x = np.random.rand(15)
+y = np.random.rand(15)
+names = np.array(list("ABCDEFGHIJKLMNO"))
+c = np.random.randint(1,5,size=15)
 
-radius = 0.1
-eps_rad = radius / kms_per_radian
+norm = plt.Normalize(1,4)
+cmap = plt.cm.RdYlGn
 
-def distance(lat1, lon1, lat2, lon2):
-    radius = 6371.0088 # km in one radian
+fig,ax = plt.subplots()
+sc = plt.scatter(x,y,c='b', s=100, cmap=cmap, norm=norm)
 
-    dlat = radians(lat2-lat1)
-    dlon = radians(lon2-lon1)
-    a = sin(dlat/2) * sin(dlat/2) + cos(radians(lat1)) \
-        * cos(radians(lat2)) * sin(dlon/2) * sin(dlon/2)
-    c = 2 * atan2(sqrt(a), sqrt(1-a))
-    d = radius * c
-    
-    return d
+annot = ax.annotate("", xy=(0,0), xytext=(20,20),textcoords="offset points",
+                    bbox=dict(boxstyle="round", fc="w"),
+                    arrowprops=dict(arrowstyle="->"))
+annot.set_visible(False)
 
-def point_index_in_cluster(lat, lon, df_clustered):
-    index = -1
+def update_annot(ind):
 
-    for i, cluster in enumerate(df_clustered.itertuples()):
-        if distance(lat, lon, cluster[1], cluster[2]) < radius:
-            return i
+    pos = sc.get_offsets()[ind["ind"][0]]
+    annot.xy = pos
+    text = "{}, {}".format(" ".join(list(map(str,ind["ind"]))), 
+                           " ".join([names[n] for n in ind["ind"]]))
+    annot.set_text(text)
+    annot.get_bbox_patch().set_facecolor('b')
+    annot.get_bbox_patch().set_alpha(0.4)
 
-    return index
-	
-df_test = pd.read_csv('./examples/example6.csv')
-df_clustered = pd.read_csv('user-location-clustered.csv')
-kPOI = 2 # afi point of interest
-aList = []
-for i in range(df_test.shape[0]):
-	aList.append(point_index_in_cluster(df_test.iloc[i]['lat'], df_test.iloc[i]['lon'], df_clustered))
-	result = distance(df_test.iloc[i]['lat'], df_test.iloc[i]['lon'], df_clustered.iloc[kPOI]['lat'], df_clustered.iloc[kPOI]['lon'] )
-    # print(result)
 
-print(aList)
+def hover(event):
+    vis = annot.get_visible()
+    if event.inaxes == ax:
+        cont, ind = sc.contains(event)
+        if cont:
+            update_annot(ind)
+            annot.set_visible(True)
+            fig.canvas.draw_idle()
+        else:
+            if vis:
+                annot.set_visible(False)
+                fig.canvas.draw_idle()
+
+fig.canvas.mpl_connect("motion_notify_event", hover)
+
+plt.show()
 
 	
