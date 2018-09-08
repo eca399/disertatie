31379db614b05fdb04d2bd50acb13cc79d58aac1
ecaterina <ecaterina.parlog@esolutions.ro>
2018-09-08 17:49:58 +0300

    clean show_graph_1.py

--- show_graph_1.py
+++ show_graph_1.py
@@ -4,15 +4,6 @@ import json
 
 with open('transition_mat_with_streets_and_points_and_info.json', 'r') as f:
     transition_mat = json.load(f)
-
-with open('transition_list.json', 'r') as f:
-    transition_list = json.load(f)
-   
-with open('sp_trans_list.json', 'r') as f:
-    sp_trans_list = json.load(f)
-    
-with open('interest_points.json', 'r') as f:
-    interest_points = json.load(f)
     
 df_gps = pd.read_csv('user_location_with_zones.csv')  
 
