9eeec50cc6eee7e9f9512014301c9664668406c7
eca399 <ecaterinaparlog@gmail.com>
2018-09-10 17:33:54 +0300

    adding too slot check

--- main2.py
+++ main2.py
@@ -1,6 +1,8 @@
 import pandas as pd, numpy as np, matplotlib.pyplot as plt, time
 import sys
 import path_eval
+import datetime
+from math import radians, cos, sin, asin, sqrt, atan2
 # from show_graph_2 import *
 
 
@@ -11,16 +13,69 @@ if (len(sys.argv) != 2):
 examplefile = sys.argv[1]
 df_test = pd.read_csv(examplefile)
 
+
+
+
+
+
+
 path = []
 x = []
 y = []
 names = []
 path = []
 for i in range(df_test.shape[0]):
-    path.append({"lat": df_test.iloc[i]['lat'], "lon": df_test.iloc[i]['lon'], "index": i})
+    path.append({"lat": df_test.iloc[i]['lat'], "lon": df_test.iloc[i]['lon'], "index": i, "time": df_test.iloc[i]['time']})
     x.append(df_test.iloc[i]['lat'])
     y.append(df_test.iloc[i]['lon'])
     names.append(str(i))
+
+def time_difference(date_time1, date_time2):
+    d1 = datetime.datetime.strptime(date_time1, '%Y-%m-%d %H:%M:%S')
+    d2 = datetime.datetime.strptime(date_time2, '%Y-%m-%d %H:%M:%S')
+    return (d2 - d1).total_seconds() / 60
+
+def distance(lat1, lon1, lat2, lon2):
+    radius = 6371.0088 # km in one radian
+
+    dlat = radians(lat2-lat1)
+    dlon = radians(lon2-lon1)
+    a = sin(dlat/2) * sin(dlat/2) + cos(radians(lat1)) \
+        * cos(radians(lat2)) * sin(dlon/2) * sin(dlon/2)
+    c = 2 * atan2(sqrt(a), sqrt(1-a))
+    d = radius * c
+    
+    return d    
+    
+def slow(path):
+
+    A = path[-1] #current point is the last point in path
+
+    B_index = "not_found" # the most recent point older than 20 mins ago
+    
+    for i in range(len(path) - 2, -1, -1): #reverse loop staring at len(path-2) ending just before -1, with an interation step of -1
+        if(time_difference(path[i]["time"], A["time"]) > 20):
+            B_index = i
+            break  # exit the loop when the first point older than 20 min is found
+            
+  
+    result = False
+    
+    if B_index == "not_found" :
+        return False
+  
+    result = True
+    for i in range(B_index, len(path) - 1):
+        Point = path[i]
+        if(distance(Point["lat"], Point["lon"], A["lat"], A["lon"]) > 0.1):
+            result = False # if all point are too near send True (is slow)
+            
+    return result
+    
+
+if slow(path):
+    print("Sending too slow alert")
+
     
 ratio = path_eval.path_length_vs_diameter(path)
 intersections, intersection_list = path_eval.intersections_count(path)
